1211dc93c16f3b0cc7c349922571a3967456e532
Matías Soto Lomboy <mati.sotitol@gmail.com>
2024-11-29 22:16:45 -0300

    Item a

--- script.r
+++ script.r
@@ -8,38 +8,35 @@ ferr <- datos$ferr
 reco <- datos$reco
 moli <- datos$moli
 
-# Diagrama de dispersión para carbono
-plot(dure, carb, xlab="Dureza del acero", ylab="Contenido de carbono")
-# Modelo lineal
-modcarb <- lm(carb~dure)
-abline(modcarb, col="blue")
-# Coeficiente de pearson
-cor(dure, carb)
-modcarb
+# Item a)
 
-# Diagrama de dispersión para ferrita
-plot(dure, ferr, xlab="Dureza del acero", ylab="Contenido de ferrita")
-# Modelo lineal
-modferr <- lm(ferr~dure)
-abline(modferr, col="blue")
-# Coeficiente de pearson
-cor(dure, ferr)
-modferr
+# Creamos una lista de variables
+variables <- list(dure = dure, carb = carb, ferr = ferr, moli = moli, reco = reco)
 
-# Diagrama de dispersión para molibdeno
-plot(dure, moli, xlab="Dureza del acero", ylab="Contenido de molibdeno")
-# Modelo lineal
-modmoli <- lm(moli~dure)
-abline(modmoli, col="blue")
-# Coeficiente de pearson
-cor(dure, moli)
-modmoli
+# Creamos una matriz de pares de variables para analizar las combinaciones
+combinaciones <- combn(names(variables), 2, simplify = FALSE)
 
-# Diagrama de dispersión para la tasa de recocido
-plot(dure, reco, xlab="Dureza del acero", ylab="Tasa de recocido")
-# Modelo lineal
-modreco <- lm(reco~dure)
-abline(modreco, col="blue")
-# Coeficiente de pearson
-cor(dure, reco)
-modreco
+# Nuestros resultados irán acá
+resultados <- data.frame(Var1 = character(0), Var2 = character(0),
+                Correlacion = numeric(0), Interpretacion = character(0))
+
+for (combinacion in combinaciones) {
+  # Extraer las dos variables para la combinación actual
+  var1 <- variables[[combinacion[1]]]
+  var2 <- variables[[combinacion[2]]]
+  
+  # Gráfico de dispersión
+  plot(var1, var2, xlab = combinacion[1], ylab = combinacion[2],
+       main = paste("Dispersión entre", combinacion[1], "y", combinacion[2]))
+  
+  # Calculamos el coeficiente de correlación de Pearson
+  cor_pearson <- cor(var1, var2)
+  
+  # Guardamos el resultado
+  resultados <- rbind(resultados, data.frame(Var1 = combinacion[1], Var2 = combinacion[2],
+        Correlacion = cor_pearson, Interpretacion = ifelse(abs(cor_pearson) > 0.8, "Altamente correlacionados",
+        ifelse(abs(cor_pearson) > 0.5, "Moderadamente correlacionados", "Baja correlación"))))
+}
+print(resultados)
+
+# Item b)
